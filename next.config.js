9cc7dc413849877630e22ddfbfe0aaf7c1d7d482
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-08-25 19:38:06 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const path = require('path')
 // Remove this if you're not using Fullcalendar features
 
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   trailingSlash: true,
   reactStrictMode: false,
   transpilePackages: [
