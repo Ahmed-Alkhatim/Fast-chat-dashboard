18c4bdc929b5a64274abe7ac7267cc4b2b8903fd
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-09-24 16:42:04 +0400

    Update OrderService.js

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,18 +1,20 @@
+import { apiURL } from "./config";
+
 class OrdersService {
 
     fetchOrders = async () => {
         try {
             const orders = await fetch('/orders', {
                 headers: {
-                    'authorization': `bearer ${localStorage.getItem('token')}`,
+                    'authorization': `bearer ${localStorage.getItem(authConfig.storageTokenKeyName)}`,
                 },
             }).then(response => response.json());
             return orders;
         } catch (error) {
-            console.error('Error fetching lessons:', error);
+            console.error('Error fetching orders:', error);
         }
     }
 }
 
-// new OrdersService().fetchOrders()
-export default new OrdersService();
+new OrdersService().fetchOrders()
+// export default new OrdersService();
